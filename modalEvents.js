24365a9300e9c0bb5541a38641302fd5d9fa7ce8
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2020-04-09 20:42:34 +0100

    Show modal only the first time

--- modalEvents.js
+++ modalEvents.js
@@ -3,15 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementsByClassName("button is-danger")[0].addEventListener('click', () => {
         document.getElementsByClassName('modal')[0].classList.toggle('is-active');
         document.getElementsByClassName('is-clipped')[0].classList.toggle('is-clipped');
-    });
-
-    document.getElementsByClassName("button is-success")[0].addEventListener('click', () => {
-        document.getElementsByClassName('is-clipped')[0].classList.toggle('is-clipped');
+        document.cookie = "First_visit=True";
     });
 
     document.getElementsByClassName("modal-close")[0].addEventListener('click', () => {
         document.getElementsByClassName('modal')[0].classList.toggle('is-active');
         document.getElementsByClassName('is-clipped')[0].classList.toggle('is-clipped');
+        document.cookie = "First_visit=True";
     });
+
+    document.getElementsByClassName("button is-success")[0].addEventListener('click', () => {
+        document.getElementsByClassName('is-clipped')[0].classList.toggle('is-clipped');
+        document.cookie = "First_visit=True";
+    });
+
+    if (document.cookie.indexOf('First_visit=') !== -1) {
+        document.getElementsByClassName('modal')[0].classList.remove('is-active');
+        document.getElementsByClassName('is-clipped')[0].classList.remove('is-clipped');
+    }
 
 });
